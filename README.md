91fea00006d14201e8e7e031535cc80630887ab9
Milos Miljkovic <milos@hyperionanalytics.com>
2015-03-10 08:32:32 -0400

    Code and image

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Add Ruler in Code Cells of Jupyter and IPython Notebook
+# Add Ruler to Code Cells of Jupyter and IPython Notebook
 
-Jupyter and IPython notebook setting enabling ruler in code cells using custom.js and custom.css files. Ruler will appear when you click-select code cell.<br>
+Jupyter and IPython notebook setting enabling ruler in code cells using custom.js and custom.css files. Ruler will appear when user click-select code cell to edit it and disappear when user goes into command mode.<br>
 
 ## Installation
 These installation instructions are for "vanilla" Jupyter and IPython notebook setup. If you have themed and customized your setup, I will assume you know how to patch this into custom.css and custom.js files.
@@ -15,3 +15,4 @@ You will overwrite two existing place-holding files with same names.<br><br>
 ## Custom settings
 * To change column at which ruler is displayed, change variable `column_number` in file **custom.js**.
 * To style ruler, change values of `.CodeMirror-ruler` CSS class in file **custom.css**.
+![alt txt](https://github.com/HyperionAnalytics/JupyterNotebook_ruler/blob/master/Jupyter_ruler "Jupyter ruler")
---
+++ custom.js
@@ -0,0 +1,58 @@
+// Custom JavaScript settings for Jupyter notebook
+
+$([IPython.events]).on('command_mode.Cell', function (event, data) {
+
+      function clearRulers(cm) {
+        for (var i = cm.display.lineSpace.childNodes.length - 1; i >= 0; i--) {
+          var node = cm.display.lineSpace.childNodes[i];
+          if (/(^|\s)CodeMirror-ruler($|\s)/.test(node.className))
+            node.parentNode.removeChild(node);
+        }
+      }
+
+      selected_cell = IPython.notebook.get_selected_cell();
+        if ((selected_cell instanceof IPython.CodeCell)) {
+            var cm = selected_cell.code_mirror;
+            clearRulers(cm);
+        }
+
+});
+
+$([IPython.events]).on('edit_mode.Cell', function (event, data) {
+
+    function setRulers(cm) {
+        var val = cm.getOption("rulers");
+        var cw = cm.defaultCharWidth();
+        var left = cm.charCoords(CodeMirror.Pos(cm.firstLine(), 0), "div").left;
+        var minH = cm.display.scroller.offsetHeight + 30;
+        for (var i = 0; i < val.length; i++) {
+          var elt = document.createElement("div");
+          elt.className = "CodeMirror-ruler";
+          var col, cls = null, conf = val[i];
+          if (typeof conf == "number") {
+            col = conf;
+          } else {
+            col = conf.column;
+            if (conf.className) elt.className += " " + conf.className;
+            if (conf.color) elt.style.borderColor = conf.color;
+            if (conf.lineStyle) elt.style.borderLeftStyle = conf.lineStyle;
+            if (conf.width) elt.style.borderLeftWidth = conf.width;
+            cls = val[i].className;
+          }
+          elt.style.left = (left + col * cw) + "px";
+          elt.style.top = "-60px";
+          elt.style.bottom = "-20px";
+          elt.style.minHeight = minH + "px";
+          cm.display.lineSpace.insertBefore(elt, cm.display.cursorDiv);
+        }
+      }
+
+    var ruler = [{column: 79}];
+
+    selected_cell = IPython.notebook.get_selected_cell();
+    if ((selected_cell instanceof IPython.CodeCell)) {
+        var cm = selected_cell.code_mirror;
+        cm.setOption('rulers', ruler);
+        setRulers(cm);
+    }
+});
